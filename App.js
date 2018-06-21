357053ba13edcd02f6d022d109b0c81062b5ce7f
DESKTOP-EQKQ4BG\Admin <ducnt@feelsyncsystem.vn>
2018-06-21 17:14:09 +0700

    english init

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello React Native!</Text>
+        <Image resizeMode="cover" source={ require('./imgs/1.jpg') } style={{width: 200, height: 200, borderRadius: 100, borderWidth: 1}} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+});
