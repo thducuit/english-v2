d99f55aa1c0e529d9080467a5732ce8ddeaffe05
DESKTOP-EQKQ4BG\Admin <ducnt@feelsyncsystem.vn>
2018-06-27 17:13:26 +0700

    create component file

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello React Native!</Text>
+        <Text>Hello React Native!!</Text>
         <Image resizeMode="cover" source={ require('./imgs/1.jpg') } style={{width: 200, height: 200, borderRadius: 100, borderWidth: 1}} />
       </View>
     );
@@ -20,3 +20,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
 });
+
---
+++ app/components/NextButton.js
@@ -0,0 +1,1 @@
+NextButton.js
